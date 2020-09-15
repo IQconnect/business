5b43bec0071ea2a78904959a68cf20975ce3f770
ralph <rafal@creativeroom.pl>
2020-09-15 09:26:49 +0200

    top animation

--- resources/views/components/top.blade.php
+++ resources/views/components/top.blade.php
@@ -7,17 +7,31 @@
     <span class="subtitle">
       Obserwuję, analizuję, wyjaśniam, tworzę, inspiruję.
     </span>
-
     <div class="top">
-      <div class="top__box">
+      <a href="#" class="top__box">
         <img src="@asset('images/img1.jpg')" alt="Doradztwo biznesowe dla firm" class="top__image"/>
-      </div>
-      <div class="top__box">
+        <div class="top__bar">
+          <h3 class="title title--small is-regular">
+            <i class="fas fa-angle-double-right"></i> Doradztwo biznesowe dla firm
+          </h3>
+        </div>
+      </a>
+      <a href="#" class="top__box">
         <img src="@asset('images/img2.jpg')" alt="Doradztwo biznesowe dla firm" class="top__image"/>
-      </div>
-      <div class="top__box">
+        <div class="top__bar top__bar--color2">
+          <h3 class="title title--small is-regular">
+            <i class="fas fa-angle-double-right"></i> Zakres usług 
+          </h3>
+        </div>
+      </a>
+      <a href="#" class="top__box">
         <img src="@asset('images/img3.jpg')" alt="Doradztwo biznesowe dla firm" class="top__image"/>
-      </div>
+        <div class="top__bar top__bar--color3">
+          <h3 class="title title--small is-regular">
+            <i class="fas fa-angle-double-right"></i> Moje wartości
+          </h3>
+        </div>
+      </a>
     </div>
   </div>
 </section>
